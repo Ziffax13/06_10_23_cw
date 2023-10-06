440b8c3d505846ded82dd404da99ef3761f534f2
ziffa <1hfghfghfg6yf2@gmail.com>
2023-10-06 14:10:02 +0300

    Add project files.

---
+++ Animals/Mylibrary.h
@@ -0,0 +1,118 @@
+﻿#pragma once
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+struct iSwim {
+    virtual void Swim() = 0;
+};
+
+struct iFly {
+    virtual void Fly() = 0;
+};
+
+class Animal  // абстрактный класс !!
+{
+protected:
+    char* name;
+    int kg;
+public:
+    Animal() = default;
+    Animal(const char* n, int k)
+    {
+        name = new char[strlen(n) + 1];
+        strcpy_s(name, strlen(n) + 1, n);
+        kg = k;
+    }
+    void Print()
+    {
+        cout << "Name: " << name << "Kg: " << kg << endl;
+    }
+    virtual void Continent() = 0;
+    virtual void Eat() = 0;
+    virtual void Sound() = 0;
+
+    ~Animal()
+    {
+        delete[] name;
+    }
+};
+
+class Pinguin : public Animal,  public iSwim
+{
+    char* continent;
+
+public:
+    Pinguin() = default;
+    Pinguin(const char* n, int kg, const char* cont) :Animal(n, kg)
+    {
+        continent = new char[strlen(cont) + 1];
+        strcpy_s(continent, strlen(cont) + 1, cont);
+    }
+
+    virtual void Eat()
+    {
+        cout << "Пингвины едят рыбу\n";
+    }
+    virtual void Sound()
+    {
+        cout << "Пингвины издают какие то звуки\n";
+    }
+    virtual void Continent()
+    {
+        cout << "Пингвины живут в Антарктиде\n";
+    }
+
+    virtual  void Swim()
+    {
+        cout << "Пингвины отлично плавают\n";
+    }
+    virtual  void Fly()
+    {
+        cout << "Пингвины отлично летают???? Ой не знаю))))\n";
+    }
+    ~Pinguin()
+    {
+        delete[] continent;
+    }
+};
+
+
+class Parrot : public Animal, public iFly
+{
+    char* continent;
+
+public:
+    Parrot() = default;
+    Parrot(const char* n, int kg, const char* cont) :Animal(n, kg)
+    {
+        continent = new char[strlen(cont) + 1];
+        strcpy_s(continent, strlen(cont) + 1, cont);
+    }
+
+    virtual void Eat()
+    {
+        cout << "Попугаи едят\n";
+    }
+    virtual void Sound()
+    {
+        cout << "Попугаи издают какие то звуки\n";
+    }
+    virtual void Continent()
+    {
+        cout << "Попугаи живут на континенте\n";
+    }
+
+    virtual  void Swim()
+    {
+        cout << "Попугаи не плавают\n";
+    }
+    virtual  void Fly()
+    {
+        cout << "Попугаи отлично летают\n";
+    }
+    ~Parrot()
+    {
+        delete[] continent;
+    }
+};
---
+++ Animals/main.cpp
@@ -0,0 +1,36 @@
+﻿#include"Mylibrary.h"
+/*дз:
+* 1.Создать класс Попугай на основе Animal, реализовать нужные интерфейсы
+* 2. Создать класс Самолет, и реализовать нужный интерфейс.
+* 3. Создать функцию, который может работать и с объектом класса самолет, и с объектом класса попугай!!
+
+*/
+
+class Ship :public iSwim {
+public:
+    virtual void Swim() {
+        cout << "Корабль - водный транспорт\n";
+    }
+};
+
+class Airplane :public iFly {
+public:
+    virtual void Fly() {
+        cout << "Самолет летит\n";
+    }
+};
+
+
+void Func(iFly* ptr) {
+    ptr->Fly();
+}
+
+int main()
+{
+    setlocale(0, "ru");
+    Parrot obj("Попугай", 100, "Континент");
+
+    Airplane pl;
+    Func(&obj);
+    Func(&pl);
+}
